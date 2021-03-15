7a9ea07f46775d0b028a0992e4226dccdf1dc194
sychus <hfernandez@neuquen.gov.ar>
2021-03-14 23:14:57 -0300

    feat(immunization): agregamos immunizations

--- L03_3_get_clinical_data_from_ips.js
+++ L03_3_get_clinical_data_from_ips.js
@@ -12,13 +12,13 @@ const fhirClient = new Client({
    }
 });
 
-function GetMedicationResultsFromIps(OneDoc) {
+function GetResultsMedicationsFromIps(OneDoc, ipsType) {
    let aux = '';
    let medicationReference = false;
    OneDoc.entry.forEach(
       (e) => {
          let oneP = e.resource;
-         if (oneP.resourceType == "MedicationStatement") {
+         if (oneP.resourceType == ipsType) {
             let medicationText = '';
             if (oneP.medicationReference) {
                let medicationRef = oneP.medicationReference.reference.split('/')[1];
@@ -39,14 +39,37 @@ function GetMedicationResultsFromIps(OneDoc) {
          }
       });
    if (medicationReference) {
-      console.log(aux);
       return aux;
    } else {
       return aux = 'Active||no-medication-info:No information about medications\n';
    }
 }
 
-async function getIPS(server, patient) {
+function GetResultsImmunizationsFromIps(OneDoc, ipsType) {
+   let aux = '';
+   let immunizations = false;
+   OneDoc.entry.forEach(
+      (e) => {
+         let oneP = e.resource;
+         if (oneP.resourceType == ipsType) {
+            immunizations = true;
+            let snomedImmunization = '';
+            oneP.vaccineCode.coding.forEach(immu => {
+               if (immu.system === 'http://snomed.info/sct') {
+                  snomedImmunization = `${immu.code}:${immu.display}`;
+               }
+            });
+            aux = `${aux}${oneP.status}|${oneP.occurrenceDateTime}|${snomedImmunization}\n`;
+         }
+      });
+   if (immunizations) {
+      return aux;
+   } else {
+      return aux = 'Error:IPS_No_Immunizations';
+   }
+}
+
+async function getIPS(server, patient, ipsType) {
    const fhirClient = new Client({
       baseUrl: server
    });
@@ -57,7 +80,11 @@ async function getIPS(server, patient) {
       for (let e of entries) {
          OneDoc = e.resource;
          if (OneDoc) {
-            aux = `${aux}${GetMedicationResultsFromIps(OneDoc)}`;
+            if (ipsType === 'Immunization') {
+               aux = `${aux}${GetResultsImmunizationsFromIps(OneDoc, ipsType)}`;
+            } else { // en este caso no tenemos otro que medications (para simplificar)
+               aux = `${aux}${GetResultsMedicationsFromIps(OneDoc, ipsType)}`;
+            }
          }
       }
    } else {
@@ -72,7 +99,7 @@ async function GetIPSMedications(server, patientidentifiersystem, patientidentif
       const patient = await fhirClient.search({ resourceType: 'Patient', searchParams: { identifier: `${patientidentifiersystem}|${patientidentifiervalue}` } });
       if (patient.total > 0) {
          let pac = patient.entry[0].resource;
-         out = await getIPS(server, pac);
+         out = await getIPS(server, pac, 'MedicationStatement');
          return out;
       } else {
          return 'Error:Patient_Not_Found'
@@ -81,110 +108,33 @@ async function GetIPSMedications(server, patientidentifiersystem, patientidentif
       return err;
    }
 }
-
-async function test() {
-   const PatientIdentifierValue = 'L03_3_T04';
-   const resultadoIPSMedication = await GetIPSMedications(baseUrl, 'http://fhirintermediate.org/patient_id', PatientIdentifierValue);
-   // const resultadoIPSImmunization = await GetIPSImmunizations(baseUrl, 'http://fhirintermediate.org/patient_id', PatientIdentifierValue);
-   // console.log(resultadoIPSMedication);
-   // console.log(resultadoIPSImmunization);
-   // return resultado;
-   return resultadoIPSMedication;
-}
-
-test();
-
 
 async function GetIPSImmunizations(server, patientidentifiersystem, patientidentifiervalue) {
    try {
       let out = '';
-      const response = await fhirClient.search({ resourceType: 'Patient', searchParams: { identifier: `${patientidentifiersystem}|${patientidentifiervalue}` } });
-      if (response.entry) {
-         const entry = response.entry;
-         for (const e of entry) {
-            let ids = e.resource.identifier;
-            for (const i of ids) {
-               let query = `http://fhir.hl7fundamentals.org/r4/Immunization?patient.identifier=${i.system}|${i.value}`;
-               const result = await axios.get(query);
-               const immunizations = result.data;
-               if (immunizations.total > 0) {
-                  const entry = immunizations.entry;
-                  entry.forEach(e => {
-                     const recurso = e.resource;
-                     out = `${out}${recurso.status}|`;
-                     let dataVaccines = recurso.vaccineCode.coding;
-                     dataVaccines.forEach(dv => {
-                        out = `${out}${dv.code}:${dv.display}|${recurso.occurrenceDateTime}\n`;
-                     });
-                  });
-               } else {
-                  out = 'Error:No_Immunizations';
-               }
-            }
-         }
+      const patient = await fhirClient.search({ resourceType: 'Patient', searchParams: { identifier: `${patientidentifiersystem}|${patientidentifiervalue}` } });
+      if (patient.total > 0) {
+         let pac = patient.entry[0].resource;
+         out = await getIPS(server, pac, 'Immunization');
          return out;
       } else {
          return 'Error:Patient_Not_Found'
       }
    } catch (err) {
-      console.log('entro por palo?', err);
       return err;
    }
 }
 
 
+async function test() {
+   const PatientIdentifierValue = 'L03_3_T04';
+   // const resultadoIPSMedication = await GetIPSMedications(baseUrl, 'http://fhirintermediate.org/patient_id', PatientIdentifierValue);
+   const resultadoIPSImmunization = await GetIPSImmunizations(baseUrl, 'http://fhirintermediate.org/patient_id', PatientIdentifierValue);
+   // console.log(resultadoIPSMedication);
+   // console.log(resultadoIPSImmunization);
+   // return resultado;
+   console.log(resultadoIPSImmunization);
+   return resultadoIPSImmunization;
+}
 
-
-/*
-const entryP = patient.entry;
-      for (const e of entryP) {
-         let id = e.id;
-         const bundle = await fhirClient.search({ resourceType: 'Bundle', searchParams: { 'composition.patient': id } });
-         console.log('bundleeeee: ', bundle);
-         if (bundle) {
-            const entradas = bundle.entry;
-            entradas.forEach(entrada => {
-               const entries = entrada.resource.entry;
-               entries.forEach(recur => {
-                  if (recur.resource) {
-                     let r = recur.resource;
-                     if (r.resourceType.toString() === 'MedicationStatement') {
-                        if (r.medicationReference) {
-                           const ref = r.medicationReference.reference;
-                           let idMedication = ref.split('/')[1];
-                           const ipsMed = {
-                              status: r.status,
-                              date: r.effectivePeriod.start,
-                              medicationID: idMedication,
-                              medication: searchMedications(entries, idMedication)
-                           };
-                           let nonexists = true;
-                           let i = 0;
-                           if (data.length) {
-                              while (i < data.length && nonexists) {
-                                 if (data[i].medicationID === ipsMed.medicationID) {
-                                    nonexists = false;
-                                 }
-                                 i++;
-                              }
-                              if (nonexists) {
-                                 data.push(ipsMed);
-                              }
-                           } else {
-                              data.push(ipsMed);
-                           }
-                        }
-                     }
-                  }
-               });
-            });
-         } else {
-            return 'Error:No_IPS';
-         }
-      }
-      data.forEach(d => {
-         out = `${out}${d.status}|${d.date}|${d.medication}\n`;
-      });
-      console.log(out);
-      return out;
-*/
+test();
